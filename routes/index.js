0637430a2239c57fed4d03546a3a5123eef63ac7
Yana Yanchenko <yana.yanchenko88@yandex.by>
2021-12-25 10:37:04 +0300

    err *

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const messages = require('../utils/messages');
 const authRoutes = require('./auth');
 const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
+const BadRequestError = require('../errors/bad-request-err');
 
 router.use('/', authRoutes);
 router.use('/users', auth, usersRoutes);
 router.use('/movies', auth, moviesRoutes);
-router.all('*', auth, () => { throw new Error(messages.err_default); });
+router.all('*', auth, () => { throw new BadRequestError(messages.err_default); });
 
 module.exports = router;
