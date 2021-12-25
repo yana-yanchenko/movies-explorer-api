ce1f988ded0e4584c617a2a22547ff32d7afd69f
Yana Yanchenko <yana.yanchenko88@yandex.by>
2021-12-25 10:43:13 +0300

    err *

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const messages = require('../utils/messages');
 const authRoutes = require('./auth');
 const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
-const BadRequestError = require('../errors/bad-request-err');
+const NotFoundError = require('../errors/not-found-err');
 
 router.use('/', authRoutes);
 router.use('/users', auth, usersRoutes);
 router.use('/movies', auth, moviesRoutes);
-router.all('*', auth, () => { throw new BadRequestError(messages.err_default); });
+router.all('*', auth, () => { throw new NotFoundError(messages.err_default); });
 
 module.exports = router;
